6b8b88eeebe0d8fcac99b6c402893ab1ba2cffac
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2023-06-29 19:41:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
 // Your script here.
+const userInput = document.getElementById('userInput');
+const startButton = document.querySelector('button');
+const countDownElement = document.getElementById('countDown');
+const endTimeElement = document.getElementById('endTime');
+
+
+
+// Function to start the countdown timer
+function startCountdown() {
+	// Get the duration from the user input
+	const duration = userInput.value;
+	// Convert duration to milliseconds
+	const durationMs = duration * 60 * 1000;
+	
+	// Get the current time and calculate the end time
+	const currentTime = new Date().getTime();
+	const endTime = new Date(currentTime + durationMs);
+	
+	// Display the end time
+	endTimeElement.textContent = 'End Time: ' + formatTime(endTime);
+	
+	// Update the countdown every second
+	let countdownInterval = setInterval(function (endTime) {
+		// Get the current time
+		let currentTime = new Date().getTime();
+		
+		// Calculate the remaining time
+		let remainingTime = endTime - currentTime;
+		
+		// let days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+		let hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		let minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+		
+		// If the remaining time is less than or equal to 0, stop the countdown
+		if (remainingTime <= 0) {
+			clearInterval(countdownInterval);
+			countDownElement.textContent = 'Countdown Complete';
+			return;
+			}
+		
+		// Display the remaining time
+		countDownElement.textContent = 'Remaining Time: ' + hours + "h "
+  + minutes + "m " + seconds + "s ";
+		}, 1000, endTime);
+	}
+
+// Function to format time as HH:MM PM/AM
+function formatTime(time) {
+  const date = new Date(time);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  const period = hours >= 12 ? 'PM' : 'AM';
+
+  hours = hours % 12;
+  hours = hours ? hours : 12; // Handle midnight (0 hours)
+
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+
+  return hours + ':' + minutes + ' ' + period;
+}
+
+// Event listener for the start button
+startButton.addEventListener('click', startCountdown);
+
+// Event listener for the Enter key in the user input field
+userInput.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    startCountdown();
+  }
+});
